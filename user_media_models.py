5359132e65bdbe62dd64138118d366565aecb336
joshuachen1 <josh526chen@gmail.com>
2019-03-12 12:55:11 -0700

    Seperate Differing Models

---
+++ user_media_models.py
@@ -0,0 +1,38 @@
+from app import db
+
+# Models Regarding the movies Database
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.VARCHAR)
+    year = db.Column(db.VARCHAR)
+    service = db.Column(db.VARCHAR)
+    tag = db.Column(db.VARCHAR)
+    url = db.Column(db.VARCHAR)
+    date_added = db.Column(db.Date)
+    image_url = db.Column(db.VARCHAR)
+
+    def __init__(self, title, year, service, tag, url, date_added, image_url):
+        self.title = title
+        self.year = year
+        self.service = service
+        self.tag = tag
+        self.url = url
+        self.date_added = date_added
+        self.image_url = image_url
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'year': self.year,
+            'service': self.service,
+            'tag': self.tag,
+            'url': self.url,
+            'date_added': self.date_added,
+            'image_url': self.image_url,
+        }
---
+++ user_models.py
@@ -0,0 +1,66 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.VARCHAR)
+    email = db.Column(db.VARCHAR)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+        }
+
+
+class Friends(db.Model):
+    __tablename__ = 'user_friend_list'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    friend_id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, user_id, friend_id):
+        self.user_id = user_id
+        self.friend_id = friend_id
+
+    def __repr__(self):
+        return '<user_id {} friend_id {}>'.format(self.user_id, self.friend_id)
+
+    def serialize(self):
+        return {
+            'user_id': self.user_id,
+            'friend_id': self.friend_id,
+        }
+
+
+class RatedMovies(db.Model):
+    __tablename__ = 'user_rated_movies'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, primary_key=True)
+    user_rating = db.Column(db.Integer)
+
+    def __init__(self, user_id, movie_id, user_rating):
+        self.user_id = user_id
+        self.movie_id = movie_id
+        self.user_rating = user_rating
+
+    def __repr__(self):
+        return '<user_id {} movie_id {} user_rating {}'.format(self.user_id, self.movie_id, self.user_rating)
+
+    def serialize(self):
+        return {
+            'user_id': self.user_id,
+            'movie_id': self.movie_id,
+            'rating': self.user_rating,
+        }
