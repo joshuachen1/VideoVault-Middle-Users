8c7ce746ea630c29d0ea4c1d1e3f6b8fb5a4b836
joshuachen1 <josh526chen@gmail.com>
2019-03-12 11:26:44 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import pymysql
+import os
+
+# Setup App
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])  # Should change based on is in Development or Production
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Enable CORS
+CORS(app)
+
+# Start Database
+db = SQLAlchemy(app)
+
+# Enable Variable Port for Heroku
+port = int(os.environ.get('PORT', 33507))
+
+# Import Models
+
+# Force pymysql to be used as replacement for MySQLdb
+pymysql.install_as_MySQLdb()
+
+
+# [url]/
+@app.route('/')
+def hello_world():
+    return 'Home Page'
+
+
+if __name__ == '__main__':
+    app.run(port=port)
