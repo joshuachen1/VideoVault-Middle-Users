4d1deafb137635c3e244d486778e681aa1279d54
joshuachen1 <josh526chen@gmail.com>
2019-03-12 11:46:56 -0700

    Created my own pseudo pagination

--- app.py
+++ app.py
@@ -42,5 +42,38 @@ def get_users():
         return str(e)
 
 
+# Pseudo Pagination
+def pseudo_paginate(page: int, list_to_paginate: []):
+    start_page = (page - 1) * app.config['POSTS_PER_PAGE']
+    end_page = start_page + app.config['POSTS_PER_PAGE']
+    if end_page > len(list_to_paginate):
+        end_page = len(list_to_paginate)
+
+    return list_to_paginate[start_page:end_page]
+
+
+# Return json
+def paginated_json(json_name: str, queried_results: [], page: int):
+    num_pages = max_pages(queried_results)
+
+    # Paginate results
+    results = pseudo_paginate(page, queried_results)
+
+    json = make_response(jsonify({json_name: [result.serialize() for result in results]}))
+    json.headers['current_page'] = page
+    json.headers['max_pages'] = num_pages
+
+    # Headers the Client is Allowed to Access
+    json.headers['Access-Control-Expose-Headers'] = 'current_page, max_pages'
+    return json
+
+
+# Return max pages for specified query
+def max_pages(queried_list: []):
+    return int(math.ceil(len(queried_list) / app.config['POSTS_PER_PAGE']))
+
+
+
+
 if __name__ == '__main__':
     app.run(port=port)
