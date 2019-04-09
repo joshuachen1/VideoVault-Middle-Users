1713cadd6cc826de3ad0d6366899317564562453
joshuachen1 <josh526chen@gmail.com>
2019-04-09 01:05:33 -0700

    Update Other Prepared Emails

--- Email.py
+++ Email.py
@@ -23,33 +23,48 @@ class Email:
         return 'Email Sent'
 
     def sub_reminder_email(self, username: str, user_email: str):
+        acc = CompanyEmail.query.filter_by(username=self.email_username).first()
+        email = '{}@gmail.com'.format(acc.username)
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(acc.username, acc.password)
+
         subject = 'Subscription Reminder'
         text = 'Hey {}, you have 2 days before your VideoVault subscription expires.\n' \
                'Remember to resubscribe to keep your shows!'.format(username)
         message = 'Subject: {}\n\n{}'.format(subject, text)
 
-        self.server.sendmail(self.email, user_email, message)
-        self.server.quit()
+        server.sendmail(email, user_email, message)
+        server.quit()
         return 'Email Sent'
 
     def movie_email(self, username: str, user_email: str, movie_title: str):
+        acc = CompanyEmail.query.filter_by(username=self.email_username).first()
+        email = '{}@gmail.com'.format(acc.username)
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(acc.username, acc.password)
+
         subject = 'Movie Rented'
         text = 'Hey {}, you have rented the movie: {}, for the next 24 hours.\n' \
                'Enjoy the movie!'.format(username, movie_title)
         message = 'Subject: {}\n\n{}'.format(subject, text)
 
-        self.server.sendmail(self.email, user_email, message)
-        self.server.quit()
+        server.sendmail(email, user_email, message)
+        server.quit()
         return 'Email Sent'
 
     def movie_return_email(self, username: str, user_email: str, movie_title: str):
+        acc = CompanyEmail.query.filter_by(username=self.email_username).first()
+        email = '{}@gmail.com'.format(acc.username)
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(acc.username, acc.password)
+
         subject = 'Movie Returned'
         text = 'Hey {}, it\'s been 24 hours!\n' \
                'The movie {} has been returned. \n'.format(username, movie_title)
         message = 'Subject: {}\n\n{}'.format(subject, text)
 
-        self.server.sendmail(self.email, user_email, message)
-        self.server.quit()
+        server.sendmail(email, user_email, message)
+        server.quit()
         return 'Email Sent'
 
 
